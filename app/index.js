0959127bb51d6015d6ce95264981a6058ad831ee
Flexpair <github.com@flexpair.com>
2024-10-27 23:20:48 +0100

    watch and listen but do not...

--- app/index.js
+++ app/index.js
@@ -286,12 +286,9 @@ class GlobalBindings {
       channelName = ""
     ) => {
       var user_roles = this.netlifyIdentity.currentUser().app_metadata.roles || [];
-      // Check if the subdomain is "demo" and add roles if necessary
-      if (window.location.hostname.split('.')[0] === "demo") {
-          // Add "watch" and "listen" roles if they are not already present
-          if (!user_roles.includes("watch")) user_roles.push("watch");
-          if (!user_roles.includes("listen")) user_roles.push("listen");
-      }      
+      // Add "watch" and "listen" roles if they are not already present
+      if (!user_roles.includes("watch")) user_roles.push("watch");
+      if (!user_roles.includes("listen")) user_roles.push("listen");   
       if (
         user_roles !== undefined &&
         (user_roles.includes("listen") || user_roles.includes("speak"))
