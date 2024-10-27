d45d1c826bc20f0d0d147c8e3d6edf9b8a8a69f9
Flexpair <github.com@flexpair.com>
2024-10-28 00:45:49 +0100

    undefined !== null

--- app/index.js
+++ app/index.js
@@ -285,12 +285,12 @@ class GlobalBindings {
       tokens = [],
       channelName = ""
     ) => {
-      var user_roles = this.netlifyIdentity.currentUser().app_metadata.roles || [];
+      var user_roles = this.netlifyIdentity.currentUser().app_metadata.roles;
+      if (user_role === undefined) user_role = []
       // Add "watch" and "listen" roles if they are not already present
       if (!user_roles.includes("watch")) user_roles.push("watch");
       if (!user_roles.includes("listen")) user_roles.push("listen");   
       if (
-        user_roles !== undefined &&
         (user_roles.includes("listen") || user_roles.includes("speak"))
       ) {
         if (this.audioContext.sampleRate == 48000) {
